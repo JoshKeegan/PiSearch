03d470cfab46e7f3ac5d83110e19a61285d3f352
Josh Keegan <josh.keegan@gmx.com>
2014-11-20 20:45:14 +0000

    Started adding some Unit Tests to find the bug(s). Mainly aimed at the suffix array search which appears to not always return the correct results near the end of the string

---
+++ UnitTests/StringSearch/CompressionTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using StringSearch;
+
+namespace UnitTests.StringSearch
+{
+    [TestFixture]
+    public class CompressionTests
+    {
+#region Four Bit Digit
+        [Test]
+        public void CompressFile4BitDigit()
+        {
+            const string STR = "1234567890";
+            const string FILE_NAME = "testCompressFile4BitDigit.";
+
+            writeStringFile(FILE_NAME + "txt", STR);
+            Compression.CompressFile4BitDigit(FILE_NAME + "txt", FILE_NAME + "4bitDigit");
+
+            string readBack = read4BitDigitFileAsString(FILE_NAME + "4bitDigit");
+
+            File.Delete(FILE_NAME + ".txt");
+            File.Delete(FILE_NAME + ".4bitDigit");
+
+            Assert.AreEqual(STR, readBack);
+            Console.WriteLine(readBack);
+        }
+
+        [Test]
+        public void CompressFile4BitDigitOddLength()
+        {
+            const string STR = "12345678905";
+            const string FILE_NAME = "testCompressFile4BitDigitOddLength.";
+
+            writeStringFile(FILE_NAME + "txt", STR);
+            Compression.CompressFile4BitDigit(FILE_NAME + "txt", FILE_NAME + "4bitDigit");
+
+            string readBack = read4BitDigitFileAsString(FILE_NAME + "4bitDigit");
+
+            File.Delete(FILE_NAME + ".txt");
+            File.Delete(FILE_NAME + ".4bitDigit");
+
+            Assert.AreEqual(STR, readBack);
+            Console.WriteLine(readBack);
+        }
+#endregion
+
+        private void writeStringFile(string filePath, string content)
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            writer.Write(content);
+            writer.Close();
+        }
+
+        private string read4BitDigitFileAsString(string filePath)
+        {
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+
+            StringBuilder builder = new StringBuilder((int)stream.Length * 2);
+
+            while(true)
+            {
+                int b = stream.ReadByte();
+
+                if(b == -1)
+                {
+                    break;
+                }
+
+                int left = b >> 4;
+                int right = b & 15; // AND 1111
+
+                builder.Append(left);
+
+                if(right != 15)
+                {
+                    builder.Append(right);
+                }
+            }
+
+            stream.Close();
+            return builder.ToString();
+        }
+    }
+}
---
+++ UnitTests/StringSearch/SearchStringTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using StringSearch;
+using SuffixArray;
+using System.IO;
+
+namespace UnitTests.StringSearch
+{
+    [TestFixture]
+    public class SearchStringTests
+    {
+        [Test]
+        public void SequentialSearch()
+        {
+            const string STR = "123456789991234";
+
+            Dictionary<string, int[]> answers = new Dictionary<string, int[]>()
+            {
+                { "1", new int[] { 0, 11 } },
+                { "2", new int[] { 1, 12 } },
+                { "12", new int[] { 0, 11 } },
+                { "5", new int[] { 4 } }
+            };
+
+            foreach(KeyValuePair<string, int[]> kvp in answers)
+            {
+                string find = kvp.Key;
+                int[] expected = kvp.Value;
+
+                int[] actual = SearchString.Search(STR, find);
+
+                CollectionAssert.AreEqual(expected, actual);
+            }
+        }
+
+        [Test]
+        public void SequentialSearchLastValue()
+        {
+            const string STR = "123456789991234";
+
+            Dictionary<string, int[]> answers = new Dictionary<string, int[]>()
+            {
+                { "4", new int[] { 3, 14 } }
+            };
+
+            foreach (KeyValuePair<string, int[]> kvp in answers)
+            {
+                string find = kvp.Key;
+                int[] expected = kvp.Value;
+
+                int[] actual = SearchString.Search(STR, find);
+
+                CollectionAssert.AreEqual(expected, actual);
+            }
+        }
+
+        [Test]
+        public void SearchSuffixArray()
+        {
+            const string STR = "123456789";
+
+            int[] suffixArray = buildSuffixArray(STR);
+            FourBitDigitArray fourBitDigitArray = convertStringTo4BitDigitArray(STR);
+
+            for(int i = 0; i < STR.Length; i++)
+            {
+                for(int j = i + 1; j <= STR.Length; j++)
+                {
+                    string find = STR.Substring(i, j - i);
+
+                    int[] seqSearchRes = SearchString.Search(STR, find);
+                    int[] suffixArraySearchRes = SearchString.Search(suffixArray, fourBitDigitArray, find);
+
+                    CollectionAssert.AreEqual(seqSearchRes, suffixArraySearchRes);
+                }
+            }
+        }
+
+        private int[] buildSuffixArray(string str)
+        {
+            //Initialise the aray that will hold the suffix array
+            int[] suffixArray = new int[str.Length];
+
+            //Calculate the suffix array
+            int status = SAIS.sufsort(str, suffixArray, str.Length);
+
+            if (status != 0)
+            {
+                string error = String.Format("Error occurred whilst generating the suffix array: {0}", status);
+                Console.WriteLine(error);
+                throw new Exception(error);
+            }
+            return suffixArray;
+        }
+
+        private FourBitDigitArray convertStringTo4BitDigitArray(string str)
+        {
+            StreamWriter writer = new StreamWriter("temp.txt");
+            writer.Write(str);
+            writer.Close();
+
+            Compression.CompressFile4BitDigit("temp.txt", "temp.4bitDigit");
+
+            Stream memStream = Compression.ReadStreamNoComression("temp.4bitDigit");
+
+            File.Delete("temp.txt");
+            File.Delete("temp.4bitDigit");
+
+            return new FourBitDigitArray(memStream);
+        }
+    }
+}
