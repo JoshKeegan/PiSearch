dd3a754f8d4968295126fe49dbecbfc33e3f8261
Josh Keegan <josh.keegan@gmx.com>
2015-01-02 20:27:15 +0000

    Imported WinFileIO and stubbed FastFileStream

---
+++ StringSearch/IO/FastFileStream.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * PiSearch
+ * FastFileStream - a fast implementation of Stream, with the underlying data being stored on a file (like FileStream)
+ * By Josh Keegan 02/01/2015
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSearch.IO
+{
+    public class FastFileStream : Stream
+    {
+        //Private variables
+        private long position;
+        private WinFileIO winFileIo;
+
+        public override bool CanRead
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool CanSeek
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool CanWrite
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
