cfa4fd6fa13e35c07664c51f72f662bc062cd197
Josh Keegan <josh.keegan@gmx.com>
2015-01-28 20:36:08 +0000

    Represent precomputed search results

---
+++ StringSearch/Collections/BigPrecomputedSearchResultsArray.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * PiSearch
+ * BigPrecomputedSearchResultsArray
+ * By Josh Keegan 28/01/2015
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringSearch.Collections
+{
+    public class BigPrecomputedSearchResultsArray : BigArray<PrecomputedSearchResult>
+    {
+        //Private vars
+        private BigArray<ulong> underlyingArray;
+
+        //Public accessors & modifiers
+        public PrecomputedSearchResult this[long i]
+        {
+            get
+            {
+                ulong minSuffixArrayIdx = underlyingArray[i * 2];
+                ulong maxSuffixArrayIdx = underlyingArray[(i * 2) + 1];
+
+                PrecomputedSearchResult result = new PrecomputedSearchResult(
+                    (long)minSuffixArrayIdx, (long)maxSuffixArrayIdx);
+                return result;
+            }
+            set
+            {
+                //Validation
+                if(value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                ulong minSuffixArrayIdx = (ulong)value.MinSuffixArrayIdx;
+                ulong maxSuffixArrayIdx = (ulong)value.MaxSuffixArrayIdx;
+
+                underlyingArray[i * 2] = minSuffixArrayIdx;
+                underlyingArray[(i * 2) + 1] = maxSuffixArrayIdx;
+            }
+        }
+
+        public long Length
+        {
+            get
+            {
+                return underlyingArray.Length / 2;
+            }
+        }
+
+        //Constructor
+        public BigPrecomputedSearchResultsArray(BigArray<ulong> underlyingArray)
+        {
+            this.underlyingArray = underlyingArray;
+        }
+
+        //Public Methods
+        public IEnumerator<PrecomputedSearchResult> GetEnumerator()
+        {
+            for(long i = 0; i < this.Length; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+    }
+}
---
+++ StringSearch/PrecomputedSearchResult.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * PiSearch
+ * Precompted Search Result - the precomputed result for a search of a specific string in a specific set of digits
+ * By Josh Keegan 28/01/2015
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringSearch
+{
+    public class PrecomputedSearchResult
+    {
+        //Public variables
+        public long MinSuffixArrayIdx { get; private set; } //Inclusive
+        public long MaxSuffixArrayIdx { get; private set; } //Exclusive
+
+        public PrecomputedSearchResult(long minSuffixArrayIdx, long maxSuffixArrayIdx)
+        {
+            //Validation
+            if(minSuffixArrayIdx < 0)
+            {
+                throw new ArgumentOutOfRangeException("minSuffixArrayIdx must be >= 0");
+            }
+
+            if(maxSuffixArrayIdx < 0)
+            {
+                throw new ArgumentOutOfRangeException("maxSuffixArrayIdx must be >= 0");
+            }
+
+            if(maxSuffixArrayIdx < minSuffixArrayIdx)
+            {
+                throw new ArgumentException("maxSuffixArrayIdx must be >= minSuffixArrayIdx");
+            }
+
+            this.MinSuffixArrayIdx = minSuffixArrayIdx;
+            this.MaxSuffixArrayIdx = maxSuffixArrayIdx;
+        }
+    }
+}
