e1589042ef475b7d001932872c67f7606014f33e
Josh Keegan <josh.keegan@gmx.com>
2019-08-05 19:01:49 +0100

    Update searches schema - missing , & integer display widths are deprecated, so remove them

--- src/StringSearch.Api/schema.sql
+++ src/StringSearch.Api/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `searches` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `find` varchar(1000) NOT NULL,
-  `minSuffixArrayIdx` bigint(20) NOT NULL,
-  `maxSuffixArrayIdx` bigint(20) NOT NULL,
-  `resultId` int(11) NOT NULL,
-  `justCount` tinyint(1) NOT NULL,
+  `minSuffixArrayIdx` bigint NOT NULL,
+  `maxSuffixArrayIdx` bigint NOT NULL,
+  `resultId` int NOT NULL,
+  `justCount` tinyint NOT NULL,
   `clientIp` varchar(39) NOT NULL COMMENT '39 for IPv6',
   `searchDate` datetime NOT NULL,
-  `processingTimeMs` bigint(20) NOT NULL,
-  `numSurroundingDigits` int(11) NULL,
-  `namedDigits` varchar(1000) NULL
+  `processingTimeMs` bigint NOT NULL,
+  `numSurroundingDigits` int NULL,
+  `namedDigits` varchar(1000) NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
