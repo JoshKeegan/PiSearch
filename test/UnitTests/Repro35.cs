717a6405b208125c003dab881d99169297b2938e
Josh Keegan <josh.keegan@gmx.com>
2019-08-06 18:30:31 +0100

    Break the unit tests to reproduce bug #35 (AppVeyor build passed when tests fail)

---
+++ test/UnitTests/Repro35.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Repro35
+    {
+        [Test]
+        public void Fail() => Assert.Fail();
+    }
+}
