403833455553657a369593abd6a00ec942981be6
Josh Keegan <josh.keegan@gmx.com>
2019-12-04 21:55:06 +0000

    Delete unused images after deployment

---
+++ deploy/deleteUnusedImage.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# Delete unused versions of an image
+#
+# Usage:
+#       ./deleteUnusedImage.sh $image
+#
+
+# Exit on error
+set -e
+
+readonly image="$1"
+
+if [ -z "$image" ]; then
+        echo "Missing required params" >&2
+        echo 'Usage: ./deleteUnusedImage.sh $image' >&2
+        exit 1
+fi
+
+# Find images that are currently being used in running containers in format ":repo1/image1:tag1:|:repo2/image2:tag2:", (ready for egrep)
+inUse=`docker container ls --format "{{ .Image }}" | \
+	grep $image: | \
+	awk 'NR != 1 { printf "|" } { printf ":" $$0 ":" }'`
+
+numInUse=`echo $inUse | wc -w`
+echo "Found $numInUse running containers using versions of this image, these will be kept"
+
+# Find images to delete (as newline delimited string)
+toDelete=`docker image ls --format ":{{ .Repository }}:{{ .Tag }}:{{ .ID }}:" | \
+		grep :$image: | \
+		grep -v -E "$inUse" | \
+		awk '{ split($$0, a, ":"); print a[4] }'`
+
+if [ -z "$toDelete" ]; 
+then
+	echo "No unused versions of this image to delete"
+else
+	numImages=`echo $toDelete | wc -w`
+	echo "Deleting $numImages unused versions of this image . . ."
+	docker rmi -f $toDelete
+fi
