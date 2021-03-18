bcc98723d73e5fed2363cc289b162913632571e5
Josh Keegan <josh.keegan@gmx.com>
2021-03-18 18:49:43 +0000

    Rip out mysql serilog sink. No longer required as centralising logs with FluentD, Elasticsearch & Kibana which this is now feeding into

--- src/StringSearch.Infrastructure/Di/HealthCheckRegistrar.cs
+++ src/StringSearch.Infrastructure/Di/HealthCheckRegistrar.cs
@@ -1,8 +1,5 @@
-﻿using System.Linq;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using StringSearch.Config;
 using StringSearch.Health;
 using StringSearch.Health.Factories;
 using StringSearch.Infrastructure.Config;
@@ -29,27 +26,6 @@ namespace StringSearch.Infrastructure.Di
                 ILogger log = provider.GetService<ILogger>();
                 MySqlConfig config = provider.GetService<MySqlConfig>();
                 return new MySqlHealthResource("Database", true, log, config.ConnectionString);
-            });
-
-            // Logging database (non-critical)
-            services.AddSingleton<IHealthResource>(provider =>
-            {
-                // Get the connection string of the Serilog MySQL sink
-                // Note: this assumes that there won't be more than one MySQL sink
-                IConfiguration config = provider.GetService<IConfiguration>();
-                string connStr = config.GetSection("Serilog")?.GetSection("WriteTo")?.GetChildren()
-                    ?.Where(s => s.GetValue<string>("Name") == "MySQL")?.FirstOrDefault()?.GetSection("Args")
-                    ?.GetValue<string>("connectionString");
-
-                if (connStr == null)
-                {
-                    // Note: null will still get registered as an IHealthResource.
-                    //  It must be filtered out whenever they are injected.
-                    return null;
-                }
-
-                ILogger log = provider.GetService<ILogger>();
-                return new MySqlHealthResource("Logs Database", false, log, connStr);
             });
 
             return services;
