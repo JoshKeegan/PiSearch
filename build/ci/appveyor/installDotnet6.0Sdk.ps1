191b530dca4ae2a4bbede4d472010b2b8931c5b6
Josh Keegan <josh.keegan@gmx.com>
2021-11-09 19:30:51 +0000

    Correct dotnet 6 sdk download path for appveyor ci

--- build/ci/appveyor/installDotnet6.0Sdk.ps1
+++ build/ci/appveyor/installDotnet6.0Sdk.ps1
@@ -5,7 +5,7 @@
 Write-Host "Installing .NET 6.0 SDK..." -ForegroundColor Cyan
 Write-Host "Downloading..."
 $exePath = "$env:TEMP\dotnet-sdk-6.0.100-win-x64.exe"
-(New-Object Net.WebClient).DownloadFile('https://dotnet.microsoft.com/download/dotnet/thank-you/sdk-6.0.100-windows-x64-installer', $exePath)
+(New-Object Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/0f71eaf1-ce85-480b-8e11-c3e2725b763a/9044bfd1c453e2215b6f9a0c224d20fe/dotnet-sdk-6.0.100-win-x64.exe', $exePath)
 Write-Host "Installing..."
 cmd /c start /wait "$exePath" /quiet /norestart
 del $exePath
